ef0833456a41b002b38b1d0013db5330e7bcbc66
Sonali16605 <79628554+Sonali16605@users.noreply.github.com>
2021-02-25 13:29:58 +0530

    Add files via upload

---
+++ Make.java
@@ -0,0 +1,19 @@
+
+import java.awt.*;
+
+public class Make{
+
+	public static void main(String[] args) {
+		Frame f1= new Frame();
+		f1.setTitle("First Frame");
+		f1.setSize(400,250);
+		f1.setVisible(true);
+
+	}
+
+}
+
+
+
+
+
